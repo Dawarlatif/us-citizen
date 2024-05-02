252d2d8e42e517b840a0b5c81dcf86457988fbf2
Dawarlatif <dawarlatif30@gmail.com>
2024-05-02 15:44:00 +0500

    env file changes

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const fastPeopleApiKey = "b9c6de8987714d199264fcb177c26291f2cbe7ee3a6";
+const FAST_PEOPLE_API_KEY = process.env.FAST_PEOPLE_API_KEY;
+const BLACK_LIST_ALLIANCE_API_KEY = process.env.BLACK_LIST_ALLIANCE_API_KEY;
+const TOKEN = process.env.TOKEN;
+const API_KEY = process.env.API_KEY;
 
 // Function to generate the API URL
 function generateApiUrl(searchTerm) {
   const baseUrl = "http://api.scrape.do";
-  const token = "b9c6de8987714d199264fcb177c26291f2cbe7ee3a6";
   const targetUrl = `https://www.fastpeoplesearch.com/${searchTerm}`;
-  return `${baseUrl}?token=${token}&url=${targetUrl}`;
+  return `${baseUrl}?token=${TOKEN}&url=${targetUrl}`;
 }
 
 export async function getFastPeopleData(searchTerm) {
@@ -23,8 +25,7 @@ export async function getFastPeopleData(searchTerm) {
 
 // Function to fetch data from Blacklist Alliance API
 export async function getBlackListAllianceData(searchTerm) {
-  const blackListAllianceApiKey = "HC3rVJPRTxVHv8fRFxWM";
-  const url = `https://YOUR_BLACKLIST_ALLIANCE_API_ENDPOINT?api_key=${blackListAllianceApiKey}&q=${searchTerm}`; // Replace with actual API endpoint
+  const url = `https://YOUR_BLACKLIST_ALLIANCE_API_ENDPOINT?api_key=${BLACK_LIST_ALLIANCE_API_KEY}&q=${searchTerm}`; // Replace with actual API endpoint
 
   // try {
   //   const response = await axios.get(url);
